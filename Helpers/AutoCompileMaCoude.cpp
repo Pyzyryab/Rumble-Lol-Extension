519a22073bc2900421bc2a0b26b1a6e85aeb970f
Alex Vergara <pyzyryab@tutanota.com>
2021-09-27 22:35:48 +0200

    Converter from hwnd to cv::Mat

---
+++ Helpers/AutoCompileMaCoude.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+/*
+    Helper that creates an executable for compile the Rumble Lol Extension C++ code when runned
+*/
+int main() {
+    // When use VSCE instead Visual Studio, normally I compile with g++, and 
+    // I need to manually link the above libraries
+    system("g++ main.cpp WindowCapture/WindowCapture.cpp -o RumbleLolExtension -lgdi32");
+    system("RumbleLolExtension.exe");
+    return 0;
+}
