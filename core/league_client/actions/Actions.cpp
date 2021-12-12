633a0a6904a5b622edc6afb575c2adea92caf43f
Alex Vergara <pyzyryab@tutanota.comC:/Users/Alex/AppData/Local/Programs/Python/Python39/python.exe>
2021-12-12 21:23:17 +0100

    Added the C++ way of provide abstract classes and interfaces to a software desing

--- core/league_client/actions/Actions.cpp
+++ core/league_client/actions/Actions.cpp
@@ -1,1 +1,16 @@
 #include "Actions.hpp"
+
+/**
+ * File that contains the Rumble predefined actions
+ * 
+ */
+
+// Simple actions
+class ClickScreen : public I_SimpleAction 
+{
+    public:
+        virtual void run_action() override 
+        {
+            // TODO Provide implementation
+        }
+};
--- core/league_client/actions/Actions.hpp
+++ core/league_client/actions/Actions.hpp
@@ -10,11 +10,16 @@ class I_Action
     public:
         // Pure virtual method definitions
         virtual void run_action() = 0;
-        virtual void get_action_identifiers() = 0;
+        // virtual void get_action_identifiers() = 0;
         virtual ~I_Action() {}
 };
 
-
+/**
+* Provides a way to interface and define what a simple action is.
+* 
+* A simple action it's a way to declare a Rumble's type of action like click 
+* on the screen, write text to the screen, scroll some sort of scrollable item...
+*/
 class I_SimpleAction : public I_Action 
 {
     private:
@@ -23,6 +28,11 @@ class I_SimpleAction : public I_Action
         virtual ~I_SimpleAction() {}
 };
 
+/**
+ * A complex action it's just the sum of many "I_SimpleAction" that performs a complex
+ * task, like the automatation of pick or ban a champ, select summoners,
+ * select a game mode directly from any place and so forth.
+ */
 class I_ComplexAction : public I_Action
 {
     private:
