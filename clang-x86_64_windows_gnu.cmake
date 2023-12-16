fbb4b037c3fcd4489731a5888e8947a8943bcc32
Alex Vergara <alex.vergara.dev@gmail.com>
2023-12-16 23:31:36 +0100

    build: cleaned the toolchain file and the CMakeLists main configuration file

--- clang-x86_64_windows_gnu.cmake
+++ clang-x86_64_windows_gnu.cmake
@@ -1,7 +1,36 @@
-set(CMAKE_C_COMPILER "C:/msys64/clang64/bin/clang.exe")
-set(CMAKE_CXX_COMPILER "C:/msys64/clang64/bin/clang++.exe")
+# ----------------------------------------------------------------------------
+#  Toolchain CMake file for configure the LLVM tools setup
+#
+#  Find more details on the README.md of the project
+#
+#  Zero Day Code, 2023
+#  
+#  Contributors:
+#   -  Alex Vergara (@Pyzyrab)
+# ----------------------------------------------------------------------------
 
-set(CMAKE_CXX_COMPILER_TARGET x86_64-windows-gnu)
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld")
-set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld")
-set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld")
+# Assuming that you already have a LLVM installation in your path...
+set(CMAKE_C_COMPILER "clang.exe")
+set(CMAKE_CXX_COMPILER "clang++.exe")
+
+# Set the triple-target
+set(TRIPLE_TARGET x86_64-windows-gnu)
+set(CMAKE_CXX_COMPILER_TARGET ${TRIPLE_TARGET})
+
+# CXX flags
+set(LIBCPP          -stdlib=libc++)
+set(CPPABI          -lc++abi)
+set(LUNWIND         -lunwind)
+set(UNWIND_TABLES   -funwind-tables)
+set(EXCEPTS         -fexceptions)
+set(RTTI            -frtti)
+
+# Append the cfg flags to the CXX flags
+set(CMAKE_CXX_FLAGS "${LIBCPP} ${CPPABI} ${LUNWIND} ${UNWIND_TABLES} ${RTTI}")
+
+# Linker flags
+set(USE_LLD -fuse-ld=lld)
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT "${USE_LLD} ${CMAKE_CXX_FLAGS}")
+set(CMAKE_MODULE_LINKER_FLAGS_INIT ${CMAKE_EXE_LINKER_FLAGS_INIT})
+set(CMAKE_SHARED_LINKER_FLAGS_INIT ${CMAKE_EXE_LINKER_FLAGS_INIT})
