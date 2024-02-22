9151924252b140547ff990eabd129252836297a3
Alex Vergara (The Rustifyer) <alex.vergara.dev@gmail.com>
2024-02-22 22:04:57 +0100

    feat: Added the clangd file for indicate that the compile_commands.json is not on the project's root

--- code/rumble_lol_plugin/helpers/string.hpp
+++ code/rumble_lol_plugin/helpers/string.hpp
@@ -1,13 +1,10 @@
 #pragma once
 
-#include <string> 
-#include <vector> 
-#include <sstream> 
-#include <codecvt>
-#include <locale>
+#include <string>
+#include <vector>
 
 namespace StringHelper {
-	std::string to_string(std::wstring wstr);
+	std::to_string(std::wstring wstr);
 	std::wstring to_wstring(std::string str);
     std::vector<std::string>& split_by_delimiter(const std::string& input, char delimiter, std::vector<std::string>& output);
 }
